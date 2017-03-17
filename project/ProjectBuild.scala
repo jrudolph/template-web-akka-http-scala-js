92fd0b4eede6056b4dbd7f1e095b7bc5dfc2e0f0
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-03-17 16:17:02 +0100

    modernize to latest versions

--- project/ProjectBuild.scala
+++ project/ProjectBuild.scala
@@ -8,6 +8,14 @@ import autoImport._
 import spray.revolver.RevolverPlugin._
 
 object ProjectBuild extends Build {
+  val akkaV = "2.4.17"
+  val akkaHttpV = "10.0.4"
+  val sprayJsonV = "1.3.3"
+  val upickleV = "0.4.4"
+  val utestV = "0.4.5"
+  val scalaJsDomV = "0.9.1"
+  val specs2V = "3.8.9"
+
   lazy val root =
     Project("root", file("."))
       .aggregate(frontend, backend)
@@ -22,8 +30,8 @@ object ProjectBuild extends Build {
         persistLauncher in Test := false,
         testFrameworks += new TestFramework("utest.runner.Framework"),
         libraryDependencies ++= Seq(
-          "org.scala-js" %%% "scalajs-dom" % "0.8.2",
-          "com.lihaoyi" %%% "utest" % "0.3.0" % "test"
+          "org.scala-js" %%% "scalajs-dom" % scalaJsDomV,
+          "com.lihaoyi" %%% "utest" % utestV % "test"
         )
       )
 
@@ -33,10 +41,10 @@ object ProjectBuild extends Build {
       .settings(commonSettings: _*)
       .settings(
         libraryDependencies ++= Seq(
-          "com.typesafe.akka" %% "akka-http-experimental" % "2.4.2",
-          "com.typesafe.akka" %% "akka-http-spray-json-experimental" % "2.4.2",
-          "io.spray" %% "spray-json" % "1.3.2",
-          "org.specs2" %% "specs2-core" % "3.7" % "test"
+          "com.typesafe.akka" %% "akka-http" % akkaHttpV,
+          "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpV,
+          "io.spray" %% "spray-json" % sprayJsonV,
+          "org.specs2" %% "specs2-core" % specs2V % "test"
         ),
         (resourceGenerators in Compile) <+=
           (fastOptJS in Compile in frontend, packageScalaJSLauncher in Compile in frontend)
@@ -45,6 +53,7 @@ object ProjectBuild extends Build {
       )
 
   def commonSettings = Seq(
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.12.1",
+    scalacOptions ++= Seq("-deprecation", "-feature", "-encoding", "utf8", "-Ywarn-dead-code", "-unchecked", "-Xlint", "-Ywarn-unused-import")
   ) ++ ScalariformSupport.formatSettings
 }
